2461de65f54015095669ad9632b86ae618633ca2
Sergey <leon@centos.local>
2021-08-16 23:27:34 +0300

    Added init folder param

--- aws_read_logs.py
+++ aws_read_logs.py
@@ -1,23 +1,26 @@
 import gzip
 import json
 import glob
+from pathlib import Path
 
 
 import argparse
 
 def print_logs():
 
-    files = glob.glob('**', recursive = True)
-
+    #Search all files in current folder and subfolders
+    files = glob.glob(f'{args.indir}\**', recursive = True)
 
     if args.eventID.upper() == "":
+        #Write CSV header
         print("NN;eventTime;awsRegion;sourceIPAddress;user;arn;eventCategory;"\
               "eventSource;eventType;eventName;eventID")
 
     count=0
-    for f in files:
-        if f.endswith(".gz"):
-            with gzip.open(f) as j:
+#    for f in files:
+    for f in Path(args.indir).rglob('*.gz'):
+        if f.name.endswith(".gz"):
+            with gzip.open(f.absolute()) as j:
                 data = json.loads( j.read() )
             if "Records" in data:
                 for i in data['Records']:
@@ -31,6 +34,7 @@ def print_logs():
                         if "arn" in i['userIdentity']:
                             arn = i['userIdentity']['arn']
 
+                    #Filter records
                     match = True
                     if not args.user in user:
                         match = False
@@ -47,10 +51,12 @@ def print_logs():
                     if  not args.event.upper() in i['eventName'].upper():
                         match = False                        
 
+                    #If finded exact eventID show json data and exit
                     if args.eventID.upper() == i['eventID'].upper():
                         print(json.dumps(i, indent=4, sort_keys=True))
                         exit(0)
 
+                    #If filter OK show record 
                     if match and args.eventID == "":
                         print(f"{count};{i['eventTime']};{i['awsRegion']};"\
                               f"{i['sourceIPAddress']};{user};{arn};{i['eventCategory']};"\
@@ -61,6 +67,7 @@ def print_logs():
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Read aws logs from .json.gz files')
+    parser.add_argument('indir', type=str, help='Folder')    
     parser.add_argument('--user',  default="", type=str, dest='user', help = "Filter by USER contains value")
     parser.add_argument('--region',  default="", type=str, dest='region', help = "Filter by region contains value")
     parser.add_argument('--ip', default="", type=str, dest='ip', help = "Filter by IP contains value")
